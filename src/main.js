d7640cb47df57d8dce8b0d1b9be56541dd77236c
Luxuan Wang <luxwan@microsoft.com>
2018-05-06 12:20:49 -0700

    react config test project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './sass/main.scss';
+//import App from './App';
+
+ReactDOM.render(
+     <div className="header">
+        React fdsfsd</div>,
+         document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const config = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle-[hash].js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool:'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)/,
+        use:[
+          'babel-loader'
+        ]
+      },{
+        test: /\.(sass|scss|css)/,
+        use: [
+        "style-loader",
+        "css-loader",
+        "resolve-url-loader",
+        "sass-loader?sourceMap"
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)/,
+        use:[
+          "file-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title:'React简易开发环境',
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config;
